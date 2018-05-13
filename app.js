d136baee6f1454b15e3c78b0f0e5d23e1bb52197
lijahthedev <lijahthedev@gmail.com>
2018-05-13 18:40:32 -0400

    Finished verison with video background and fixed navigation

---
+++ app.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $('body').show();
+
+    $(".mobile-inner-header-icon").click(function(){
+        $(this).toggleClass("mobile-inner-header-icon-click mobile-inner-header-icon-out");
+        $(".mobile-inner-nav").slideToggle(250);
+        
+    });
+    
+      $(".mobile-inner-nav a").each(function( index ) {
+          $( this ).css({'animation-delay': (index/10)+'s'});
+      });
+ });
